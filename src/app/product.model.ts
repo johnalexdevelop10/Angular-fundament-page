872ad7488c94abbb317d33cc87ad657f1bbb3ac7
johnalexdeveolp10 <johnzeta10@gmail.com>
2023-01-14 08:53:33 -0500

    first

---
+++ src/app/product.model.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  name: string;
+  price: number;
+  image: string;
+  category?: string;
+}
+//interface = es la forma de decir cuales son los atributos que debe tener cada objeto
